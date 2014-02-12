0edf2a1bdc9ff2331e968e53df532a4c0bfec926
LoganJones2 <Logan.Jones@cameron.edu>
2014-02-12 14:49:12 -0600

    Create main.cpp
    Driver for Queue.h

---
+++ main.cpp
@@ -0,0 +1,30 @@
+#inlcude <iostream>
+#include "Queue.h"
+using namespace std;
+
+int main(){
+  //create Queue object
+  Queue q;
+  
+  //test various functions of Queue object
+  //test push function by adding 5 numbers
+  for(int i=0; i<5; i++){
+    q.push((i+10)*5);
+  }
+  
+  //output front and back to check respective function
+  cout << "Front is: " << q.get_front() << endl;
+  cout << "Back is: " << q.get_back() << endl;
+  
+  //push another object and pop front object
+  q.push(-25);
+  q.pop();
+  
+  //output size.  Should be 5.  Then get new front and back.
+  cout << "Size is: " << q.get_size() << endl;
+  cout << "Front is: " << q.get_front() << endl;
+  cout << "Back is: " << q.get_back() << endl;
+  
+  //end program
+  return 0;
+}
